7bb9af4cbdd57c90f0f789312c3df00bc1f8592c
Nilusha Wimalasena <nw.nilusha@gmail.com>
2025-08-23 11:41:50 +0100

    Crypto Prices Initial Code

---
+++ CryptoPrices/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoPrices
+//
+//  Created by Nilusha Niwanthaka Wimalasena on 23/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoPrices/CryptoPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPricesApp.swift
+//  CryptoPrices
+//
+//  Created by Nilusha Niwanthaka Wimalasena on 23/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
